35864a8fd4f9e5d2dfc9a0dde25b68614c1a3b98
USHER-PB <ushertchankoumi9@gmail.com>
2025-11-21 20:13:12 +0100

    Add Testimonials component to main page layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
-'use client'
-
-import { useState } from 'react'
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
 import About from '@/components/About'
 import Skills from '@/components/Skills'
 import Experience from '@/components/Experience'
+import Testimonials from '@/components/Testimonials'
 import Contact from '@/components/Contact'
 import Footer from '@/components/Footer'
 
@@ -17,6 +15,7 @@ export default function Home() {
       <About />
       <Skills />
       <Experience />
+      <Testimonials />
       <Contact />
       <Footer />
     </main>
